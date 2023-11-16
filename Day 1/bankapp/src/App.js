3c723e12ec7bd2bbd60af9318e4ead82a358f86d
Artiya-S-U-21 <125013822+Artiya-S-U-21@users.noreply.github.com>
2023-11-16 12:11:20 +0530

    Day 1

---
+++ Day 1/bankapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Helloworld from './components/Helloworld';
+import Greetings from './components/Greetings';
+import HelloWorldd from './components/HelloWorldd';
+function App()
+{
+  return(
+    <div className="App">
+      <Helloworld />
+      <Greetings />
+      <HelloWorldd />
+    </div>
+  );
+}
+export default App;
